61283a7df517bd29ffe34ee91c4b24571bdb1ca3
Anish-Gupta26 <anishgupta2610@gmail.com>
2021-10-19 16:06:55 +0530

    All functionality added

--- mysite/fileupload/views.py
+++ mysite/fileupload/views.py
@@ -8,35 +8,32 @@ from .models import FileUpload
 #Global variable
 dict={}         #Stores matching pairs of placeholders
 size_Pholder=0  #Size/ Number of placeholder in .txt file
+col_head=[]     #Stores column header of excel file
 output=[]       #List of Final output strings with data from excel
 txt_data=""     #String with data of .txt file
 df=""           #Data Frame object of pandas
 
-def send_mail(request):
+def send_mail(request):     #Function to send mail 
     if request.method == "POST":
-        # s_email = request.POST.get["e-mail"]  #Extracting data from excel file
-        # s_passwsord = request.POST.get['password']
-        # subject = request.POST.get['subject']
-        subject = "12.35"
-        # print(s_email,s_passwsord)
+        s_email = request.POST.get("e-mail")  #Extracting data from excel file
+        s_passwsord = request.POST.get('password')
+        subject = request.POST.get('subject')
+        email_col = request.POST.get('email_col')
         for i in range(len(output)):
             port = 587  # For starttls
             smtp_server = "smtp.gmail.com"
-            sender_email = "jokerr2610@gmail.com"
-            receiver_email = df.loc[df.index[i],"email"]
-            # password = input("Type your password and press enter:")
-            password = "anish2610"
+            sender_email = s_email
+            receiver_email = df.loc[df.index[i],email_col]
+            password = s_passwsord
             message = 'Subject: {}\n\n{}'.format(subject, output[i])
             context = ssl.create_default_context()
             with smtplib.SMTP(smtp_server, port) as server:
-                server.ehlo()  # Can be omitted
+                server.ehlo()  
                 server.starttls(context=context)
-                server.ehlo()  # Can be omitted
+                server.ehlo() 
                 server.login(sender_email, password)
                 server.sendmail(sender_email, receiver_email, message)
-    return render(request,'output.html',{'output':output})
-    # return HttpResponse("Mail sent successfully!")
-    
+    return render(request,'output.html')    
 
 def matching(request):
     if request.method == "POST":
@@ -55,27 +52,22 @@ def matching(request):
                 data = df.loc[df.index[i],dict[keys]]    #data extraction
                 temp = temp.replace(keys, str(data))     #replacing placeholder with excel data
             output.append(temp)
-    
-    return render(request,'details.html') #Rendering output.html using final output strings
-    # send_mail()
-    # return render(request,'output.html',{'output':output}) #Rendering output.html using final output strings
-
+    return render(request,'details.html',{'col_head':col_head}) #INput user email and password
 
 def home(request):
     if request.method == "POST":
         excel_file = request.FILES["excel-file"]  #Extracting data from excel file
         text_file = request.FILES['text-file']    #Extracting data from .txt file
-
         #Checking for valid file format
-        if text_file.name.endswith('.txt')==False or excel_file.name.endswith('.csv')==False:
+        valid_excel = (".xls",".xlsx",".xlsm" ,".xlsb",".odf" ,".ods","odt")
+        if text_file.name.endswith('.txt')==False or excel_file.name.endswith(valid_excel)==False:
              return HttpResponse("Oops! Please provide right file format")
         
-        global df, txt_data,size_Pholder 
+        global df, txt_data,size_Pholder,col_head
         df=""
         txt_data=""
-        csv = pd.read_csv(excel_file) 
+        csv = pd.read_excel(excel_file) 
         df = pd.DataFrame(csv)
-        
         col_head = []                    #Headers of excel file
         for col in csv.columns:
             col_head.append(col)
@@ -86,17 +78,8 @@ def home(request):
         text_Pholder = re.findall('\%.*?\%', txt_data) #Placeholders of .txt file using regex
         size_Pholder = len(text_Pholder)               #Total placeholders
         return render(request,'index.html',{'something':True,'text_Pholder':text_Pholder,'excel_Pholder':col_head,'range':range(size_Pholder), 'count':100})
-
     return render(request,'index.html')
 
 def upload(request):
     return render(request,"fileupload.html")
 
-#For uploading into DB
-# Create your views here.
-# def home(request):
-#     if request.method == "POST":
-#         file2 = request.FILES["file"]
-#         document= FileUpload.objects.create(file=file2)
-#         return HttpResponse("Your File was uploaded")
-#     return render(request,'index.html')
